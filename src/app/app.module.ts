aef832d2b1b8a2e0617e01ab52fc86bf63a55d84
Brooke Kullberg <brookekullberg@gmail.com>
2019-06-21 08:32:32 -0700

    add imports, declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ArticleFirstComponent } from './article-first/article-first.component';
+import { ArticleSecondComponent } from './article-second/article-second.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ArticleFirstComponent,
+    ArticleSecondComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
